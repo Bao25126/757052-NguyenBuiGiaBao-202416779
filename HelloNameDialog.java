2a29962c4b749d34463bd4454a559e8d642a5ed0
Bao25126 <merjason1617@gmail.com>
2025-10-02 14:03:58 +0700

    Delete all files

--- HelloNameDialog.java
+++
@@ -1,8 +0,0 @@
-import javax.swing.JOptionPane;
-
-public class HelloNameDialog {
-    public static void main(String[] args) {
-        String name = JOptionPane.showInputDialog("Input your name:");
-        JOptionPane.showMessageDialog(null, "Hello " + name + "!");
-    }
-}
